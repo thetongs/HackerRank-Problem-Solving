b3ba0e1b53f722c5f2894b547cc8f1ac7767fa6c
Tongs <62232130+thetongs@users.noreply.github.com>
2022-05-11 11:19:25 +0530

    Create migratory_birds.py

---
+++ migratory_birds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    arr = sorted(arr)
+    data = Counter(arr)
+    
+    return data.most_common(1)[0][0]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
