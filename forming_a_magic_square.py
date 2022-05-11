cfb06c1205d45dda8fc25b0710e296b5015c1295
Tongs <62232130+thetongs@users.noreply.github.com>
2022-05-11 12:49:07 +0530

    Update forming_a_magic_square.py

--- forming_a_magic_square.py
+++ forming_a_magic_square.py
@@ -1,40 +1,33 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
 import sys
 
-#
-# Complete the 'formingMagicSquare' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY s as parameter.
-#
+pre = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+        ]
 
 def formingMagicSquare(s):
-    # Write your code here
-    s = np.array(s)
-    ans = 0
-    remin = (sum(s) + ans) / 9
-    while(type(remin) == int):
-        remin = (sum(s) + ans) / 9
-        ans = ans + 1
-                
-    return ans
+    results = []
+    for p in pre:
+        total = 0
+        for p_row, s_row in zip(p, s):
+            for i, j in zip(p_row, s_row):
+                if not i == j:
+                    total += max([i, j]) - min([i, j])
+            
+        results.append(total)
         
+    return min(results)
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+if __name__ == "__main__":
     s = []
-
-    for _ in range(3):
-        s.append(list(map(int, input().rstrip().split())))
-
+    for s_i in range(3):
+        s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
+        s.append(s_t)
     result = formingMagicSquare(s)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
