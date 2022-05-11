25b91c5a678dce5ad51e09f0454360b308e84d14
Tongs <62232130+thetongs@users.noreply.github.com>
2022-05-11 12:42:59 +0530

    Create forming_a_magic_square.py

---
+++ forming_a_magic_square.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s):
+    # Write your code here
+    s = np.array(s)
+    ans = 0
+    remin = (sum(s) + ans) / 9
+    while(type(remin) == int):
+        remin = (sum(s) + ans) / 9
+        ans = ans + 1
+                
+    return ans
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
